c293c2e804e53cc5b1f2981358048779a06187f8
Alfred <alfred.svenblad@ha.ax>
2026-01-09 10:05:18 +0200

    some raw js

---
+++ if.js
@@ -0,0 +1,31 @@
+//IF satser används för selektion
+
+let temp =-3;
+
+if (temp>24){
+    console.log("Leif badar.")
+}else if(temp<0){
+    console.log("Leif åker skridskor.");
+}
+else{
+    console.log("Leif badar inte.")
+}
+
+let enter=false;
+
+if(enter){
+    console.log("welcome in.");
+}else{
+    console.log("you may not pass.");
+}
+
+let namn="Darby";
+
+if(namn.toUpperCase()=="DARBY"){
+    console.log("du är inloggad.");
+}else{
+    console.log("du är inte inloggad.");
+}
+
+
+
